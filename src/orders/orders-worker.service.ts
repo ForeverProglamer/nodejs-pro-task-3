4199b120493525265c51896b47fe12f43d8bd738
Bohdan Chernykh <dev@bchernykh.anonaddy.com>
2026-03-16 18:39:35 +0200

    feat(worker): centralize configuration, provide enable/disable flag

--- src/orders/orders-worker.service.ts
+++ src/orders/orders-worker.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Logger, OnApplicationBootstrap } from "@nestjs/common";
 import { OrdersService } from "./orders.service";
 import { AckCallback, RabbitMqService } from "src/rabbit-mq/rabbit-mq.service";
 import { ProcessOrderMessageDto } from "./order-processed-message.dto";
+import { ConfigService } from "@nestjs/config";
+import { parseBoolean } from "src/common/utils";
 
 @Injectable()
 export class OrdersWorkerService implements OnApplicationBootstrap {
@@ -10,9 +12,22 @@ export class OrdersWorkerService implements OnApplicationBootstrap {
   constructor(
     private readonly ordersService: OrdersService,
     private readonly rabbitmq: RabbitMqService,
+    private readonly configService: ConfigService,
   ) {}
 
+  getConfig() {
+    return {
+      enableWorker: parseBoolean(
+        this.configService.get("WORKER_ENABLE") || "false",
+      ),
+      maxRetries: Number(this.configService.get("WORKER_RETRIES") || "3"),
+      backoffSeconds: Number(this.configService.get("WORKER_BACKOFF_S") || "5"),
+    };
+  }
+
   async onApplicationBootstrap() {
+    const { enableWorker } = this.getConfig();
+    if (!enableWorker) return;
     await this.rabbitmq.consume("orders.process", this.handle.bind(this));
     this.logger.log(`Worker has been initialized`);
   }
