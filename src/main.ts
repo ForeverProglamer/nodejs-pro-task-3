398f5b0bfdf4447474a5b5640326c2902877ba21
Bohdan Chernykh <dev@bchernykh.anonaddy.com>
2026-03-20 03:11:48 +0200

    feat: dev container with hot reload

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { HttpExceptionFilter } from "./common/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
+
+  // Help dev restarts inside containers release the port promptly.
+  const shutdown = async () => {
+    await app.close();
+    process.exit(0);
+  };
+
+  process.once("SIGTERM", shutdown);
+  process.once("SIGINT", shutdown);
 }
 
 bootstrap();
