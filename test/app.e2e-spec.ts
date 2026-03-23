3a64f6bbcf90f8730a18cf0fbfb5edc74b1e2513
Bohdan Chernykh <dev@bchernykh.anonaddy.com>
2026-03-23 23:01:44 +0200

    feat: add support for tests

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { Test } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { AppModule } from "../src/app.module";
+
+describe("App (e2e)", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it("/ (GET)", () => {
+    return request(app.getHttpServer()).get("/").expect(200);
+  });
+});
