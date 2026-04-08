7c5be4135b99a5ce7a101f363a3da441b01a9c5c
Bohdan Chernykh <dev@bchernykh.anonaddy.com>
2026-04-08 23:23:46 +0300

    fix(test): placeholder test of users.service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { UsersService } from "./users.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "src/auth/user.decorator";
 
-describe('UsersService', () => {
+describe("UsersService", () => {
   let service: UsersService;
+
+  const repo = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: repo },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
