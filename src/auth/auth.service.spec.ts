2862cf18d64d0c7dcca482b4ee3220227c97329c
Bohdan Chernykh <dev@bchernykh.anonaddy.com>
2026-04-09 15:46:25 +0300

    fix(test): adapt auth.service test to handle new provider

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -11,15 +11,19 @@ import {
 import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { randomUUID } from "crypto";
+import { PasswordService } from "./password.service";
+
+const reverse = (s: string) => s.split("").reverse().join("");
 
 const johnDoeId = randomUUID();
 const johnDoeEmail = "johh.doe@mail.com";
+const johnDoeRawPass = "john-doe-pass";
+
 const johnDoe: Partial<User> = {
   id: johnDoeId,
   email: johnDoeEmail,
-  password: "$2b$12$Ql4xM709i0nz3tOLezOwguHF3uU8HCcA7.CUcPwG2MerwCXyfmdQG",
+  password: reverse(johnDoeRawPass),
 };
-const johnDoePass = "john-doe-pass";
 
 class MockUsersService {
   private repo: Partial<User>[];
@@ -57,6 +61,11 @@ describe("AuthService", () => {
     get: jest.fn(),
   };
 
+  const passwordService = {
+    hash: jest.fn(reverse),
+    verify: jest.fn((v1, v2) => v1 === reverse(v2)),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -64,22 +73,32 @@ describe("AuthService", () => {
         { provide: UsersService, useClass: MockUsersService },
         { provide: JwtService, useValue: jwtService },
         { provide: ConfigService, useValue: configService },
+        { provide: PasswordService, useValue: passwordService },
       ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
 
     jwtService.sign.mockClear();
+    passwordService.hash.mockClear();
+    passwordService.verify.mockClear();
   });
 
   describe(".login", () => {
     it("returns access token response for successful log in", async () => {
-      const dto = { username: johnDoeEmail, password: johnDoePass };
+      const dto = { username: johnDoeEmail, password: johnDoeRawPass };
       const result = await service.login(dto);
       expect(result.accessToken).toBeDefined();
       expect(result.refreshToken).toBeDefined();
       expect(result.tokenType).toBe("Bearer");
       expect(result.expiresIn).toBeDefined();
+
+      // Verifies password, uses right args for the call
+      expect(passwordService.verify.mock.calls.length).toBe(1);
+      expect(passwordService.verify.mock.calls[0]).toEqual([
+        johnDoe.password,
+        dto.password,
+      ]);
 
       // Signs access and refresh tokens
       expect(jwtService.sign.mock.calls.length).toBe(2);
